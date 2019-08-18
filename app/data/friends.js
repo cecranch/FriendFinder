362f71f19cf8ceaf7472fa5e6f4b65351a859c5f
Caroline Cran <cranch.caroline@gmail.com>
2019-08-17 23:44:21 -0400

    html and css complete. survey questions and friend data complete. moving on to functionality

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. 
+// Each of these objects should roughly follow the format below.
+
+
+var friends = [
+{
+    name:"Jen",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        4,
+        1,
+        3,
+        2,
+        1,
+        4,
+        3,
+        5,
+        2,
+        3
+      ]
+  },
+  {
+    name:"George",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        3,
+        1,
+        5,
+        4,
+        2,
+        1,
+        3,
+        2,
+        4,
+        5
+      ]
+  },
+  {
+    name:"Rebel",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        1,
+        1,
+        2,
+        4,
+        3,
+        5,
+        5,
+        5,
+        2,
+        1
+      ]
+  },
+  {
+    name:"Bruce",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        5,
+        3,
+        1,
+        2,
+        1,
+        1,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    name:"Chris",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        2,
+        4,
+        4,
+        4,
+        2,
+        5,
+        3,
+        1
+      ]
+  }
+];
+  // Export to API Route
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,14 @@
+// Your server.js file should require the basic npm packages we've used in class: express and path.
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
