cb2e9f654281b814ac6a4518c45b65aae2d59174
Caroline Cran <cranch.caroline@gmail.com>
2019-08-19 17:09:25 -0400

    updating get and post routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,16 @@ var path = require("path");
 
 
 //  apiRoutes.js file should contain two routes:
-
+module.exports = function (app) {
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic.
+  app.get("/api/friends", function(req, res) {  
+    console.log(res);
+    return res.json(friends);
+  });
 
+  // A POST routes /api/friends. This will be used to handle incoming survey results. 
+  // This route will also be used to handle the compatibility logic.
+
+  app.post("/api/friends", function(req, res) {  
+    console.log(res);
+  });
